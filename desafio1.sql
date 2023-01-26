77df68093af24a7d65a1f1d880b66225e3cf169a
Joana <joanamdsantos1@gmail.com>
2023-01-26 18:33:11 -0300

    definindo os nomes das tabelas e já criando algumas

--- desafio1.sql
+++ desafio1.sql
@@ -2,25 +2,40 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
   CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
+  CREATE TABLE SpotifyClone.signatures(
+      sign_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+      sign_name VARCHAR(50) NOT NULL,
+      sign_value FLOAT NOT NULL
   ) engine = InnoDB;
 
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
+  CREATE TABLE SpotifyClone.artists(
+      artist_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+      artist_name VARCHAR(200) NOT NULL
+  ) engine = InnoDB;
+  
+  CREATE TABLE SpotifyClone.users(
+      user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+      user_name VARCHAR(200) NOT NULL,
+      user_age INT NOT NULL,
+      sign_date DATE NOT NULL,
+      sign_id INT NOT NULL, 
+      FOREIGN KEY (sign_id) REFERENCES signatures (sign_id)
+  ) engine = InnoDB;
+  
+  CREATE TABLE SpotifyClone.following_artists(
+      user_id INT NOT NULL,
+      artist_id INT NOT NULL, 
+      FOREIGN KEY (user_id) REFERENCES users (user_id),
+      FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
   ) engine = InnoDB;
 
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+  -- INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
+  -- VALUES
+  --   ('exemplo de dados 1', 'exemplo de dados A'),
+  --   ('exemplo de dados 2', 'exemplo de dados B'),
+  --   ('exemplo de dados 3', 'exemplo de dados C');
 
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+  -- INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
+  -- VALUES
+  --   ('exemplo de dados 1', 'exemplo de dados X'),
+  --   ('exemplo de dados 2', 'exemplo de dados Y');
