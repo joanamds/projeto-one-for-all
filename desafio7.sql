50ce55579a28ecf0ae05746c07b8d1c4f5e8fb67
Joana <joanamdsantos1@gmail.com>
2023-01-27 15:24:23 -0300

    fix: consertando a ordem da tabela

--- desafio7.sql
+++ desafio7.sql
@@ -6,4 +6,4 @@ FROM SpotifyClone.artists AS ar
 INNER JOIN SpotifyClone.following_artists AS fa ON ar.artist_id = fa.artist_id
 INNER JOIN SpotifyClone.album AS al ON al.artist_id = ar.artist_id
 GROUP BY artista, album
-ORDER BY seguidores DESC, album;
+ORDER BY seguidores DESC, artista, album;
