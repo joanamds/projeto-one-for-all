e7ecf047d19e45c853db5c055f48495bb77cc7e2
Joana <joanamdsantos1@gmail.com>
2023-01-27 15:20:56 -0300

    desafio 7 finalizado

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT 
+  ar.artist_name AS artista,
+  al.album_name AS album,
+  COUNT(fa.artist_id) AS seguidores
+FROM SpotifyClone.artists AS ar
+INNER JOIN SpotifyClone.following_artists AS fa ON ar.artist_id = fa.artist_id
+INNER JOIN SpotifyClone.album AS al ON al.artist_id = ar.artist_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, album;
